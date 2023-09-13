afe20146bae1e3922817babb2eeb030d92c933ad
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-09-13 10:42:03 +0300

    first commit

---
+++ BlazorClient_Todo/Client/Models/TodoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorClient_Todo.Client.Models
+{
+    public class TodoDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
