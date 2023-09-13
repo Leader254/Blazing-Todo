75ffca3a994bb2752f371b7448d5a4f348f92594
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-09-13 16:20:08 +0300

    update working

--- BlazorClient_Todo/Client/Models/TodoDto.cs
+++ BlazorClient_Todo/Client/Models/TodoDto.cs
@@ -2,7 +2,7 @@
 {
     public class TodoDto
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
